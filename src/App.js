4867083693472b59c62156fe12c71e6f1c70bb2d
Gautham-24 <gauthamraj2401@gmail.com>
2024-07-25 17:52:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Navbar from "./components/Navbar";
 import Grid from "./components/Grid";
 
 function App() {
+  const apiKey = 'e3fc791db87347d7a982ccc92e1f1427';
+  // const apiKey = 'd2dd3a1fc33e42b88e476744a2ee5d52';
+  const pageSize = 12;
   document.body.style.backgroundColor = "#374151";
   document.body.style.color = "white";
   return (
     <>
       <Navbar />
-      <Grid/>
+      <Grid apiKey={apiKey} pageSize={pageSize}/>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Navbar() {
   return (
     
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 ">
+    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">NewzzApp</span>
         </a>
