289d820b4bcf4651c9ccf1a110ae20c78ba0e44a
Gautham-24 <gauthamraj2401@gmail.com>
2024-07-26 11:58:02 +0530

    commit

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,56 +1,103 @@
-import React, { useEffect, useState } from 'react'
-import Card from './Card';
-import Loading from './Loading';
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import Loading from "./Loading";
 
 export default function Grid(props) {
-    const [articles,setArticles] = useState([]);
-    const [totalResults,setTotalResults] = useState(0);
-    const [page,setPage] = useState(1);
-    const [loading,setLoading] = useState(true);
+  const [articles, setArticles] = useState([]);
+  const [totalResults, setTotalResults] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  let country;
+  switch (props.country) {
+    case "in":
+      country = "India";
+      break;
+    case "us":
+      country = "USA";
+      break;
+    case "in":
+      country = "India";
+      break;
+    default:
+      country = "India";
+      break;
+  }
 
-    const update = async ()=>{
-      setLoading(true);
-      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`
-      let data = await fetch(url);
-      let parsedData = await data.json()
-      setArticles(parsedData.articles)
-      setTotalResults(parsedData.totalResults)
-      setLoading(false);
+  const update = async () => {
+    setLoading(true);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    update();
+  }, [page]);
+
+  const prev = () => {
+    if (page > 1) {
+      setPage(page - 1);
     }
+  };
 
-    useEffect(() => {
-      update(); 
-    }, [page]);
-
-    const prev = ()=>{
-      if (page > 1) {  
-        setPage(page - 1);  
-      }  
+  const next = () => {
+    if (page < Math.ceil(totalResults / props.pageSize)) {
+      setPage(page + 1);
     }
+  };
 
-    const next = ()=>{
-      if (page < Math.ceil(totalResults / props.pageSize)) {  
-        setPage(page + 1);  
-      }  
-    }
+  const capitalize = (word) => {
+    return word[0].toUpperCase() + word.slice(1);
+  };
+
   return (
-      <div>
-        {loading? <Loading/>:
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
         <>
-        <h1 className='text-5xl text-center font-mono my-2'>Newzz</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 p-2">
-          {articles.map((element)=>{
-              return<div  key={element.url}>
-              <Card title={element.title} desc={element.description} img={element.urlToImage} source={element.url}/>
-              </div>        
-          })}
-        </div>
-        <div className="flex justify-between mx-5">
-          <button type="button" onClick={prev} disabled={page===1} className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Previous</button>
-          <button type="button" onClick={next} disabled={page>=Math.ceil(totalResults/props.pageSize)} className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Next</button>
-        </div>
+          <h1 className="text-5xl text-center font-mono my-2">
+            Newzz - Top{" "}
+            {props.category === "general" ? "" : capitalize(props.category)}{" "}
+            News In {country}
+          </h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 p-2">
+            {articles.map((element) => {
+              return (
+                <div key={element.url}>
+                  <Card
+                    title={element.title}
+                    desc={element.description}
+                    img={element.urlToImage}
+                    source={element.url}
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <div className="flex justify-between mx-5">
+            <button
+              type="button"
+              onClick={prev}
+              disabled={page === 1}
+              className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            >
+              Previous
+            </button>
+            <button
+              type="button"
+              onClick={next}
+              disabled={page >= Math.ceil(totalResults / props.pageSize)}
+              className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            >
+              Next
+            </button>
+          </div>
         </>
-        }
-        </div>
-  )
+      )}
+    </div>
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar(props) {
-  const [dropdownOpen, setDropdownOpen] = useState(false)
-  const location = useLocation()
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const location = useLocation();
 
   const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen)
-  }
+    setDropdownOpen(!dropdownOpen);
+  };
 
   return (
     <nav className="sticky top-0 z-50 bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">NewzzApp</span>
+        <Link
+          to="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            NewzzApp
+          </span>
         </Link>
         <button
           data-collapse-toggle="navbar-default"
@@ -44,7 +49,11 @@ export default function Navbar(props) {
             <li>
               <Link
                 to="/"
-                className={`block py-2 px-3 rounded ${location.pathname === '/' ? 'text-blue-700 dark:text-blue-500' : 'text-gray-900 dark:text-white'} hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
+                className={`block py-2 px-3 rounded ${
+                  location.pathname === "/"
+                    ? "text-blue-700 dark:text-blue-500"
+                    : "text-gray-900 dark:text-white"
+                } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
               >
                 General
               </Link>
@@ -52,7 +61,11 @@ export default function Navbar(props) {
             <li>
               <Link
                 to="/business"
-                className={`block py-2 px-3 rounded ${location.pathname === '/business' ? 'text-blue-700 dark:text-blue-500' : 'text-gray-900 dark:text-white'} hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
+                className={`block py-2 px-3 rounded ${
+                  location.pathname === "/business"
+                    ? "text-blue-700 dark:text-blue-500"
+                    : "text-gray-900 dark:text-white"
+                } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
               >
                 Business
               </Link>
@@ -60,7 +73,11 @@ export default function Navbar(props) {
             <li>
               <Link
                 to="/entertainment"
-                className={`block py-2 px-3 rounded ${location.pathname === '/entertainment' ? 'text-blue-700 dark:text-blue-500' : 'text-gray-900 dark:text-white'} hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
+                className={`block py-2 px-3 rounded ${
+                  location.pathname === "/entertainment"
+                    ? "text-blue-700 dark:text-blue-500"
+                    : "text-gray-900 dark:text-white"
+                } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
               >
                 Entertainment
               </Link>
@@ -68,7 +85,11 @@ export default function Navbar(props) {
             <li>
               <Link
                 to="/health"
-                className={`block py-2 px-3 rounded ${location.pathname === '/health' ? 'text-blue-700 dark:text-blue-500' : 'text-gray-900 dark:text-white'} hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
+                className={`block py-2 px-3 rounded ${
+                  location.pathname === "/health"
+                    ? "text-blue-700 dark:text-blue-500"
+                    : "text-gray-900 dark:text-white"
+                } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
               >
                 Health
               </Link>
@@ -76,7 +97,11 @@ export default function Navbar(props) {
             <li>
               <Link
                 to="/science"
-                className={`block py-2 px-3 rounded ${location.pathname === '/science' ? 'text-blue-700 dark:text-blue-500' : 'text-gray-900 dark:text-white'} hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
+                className={`block py-2 px-3 rounded ${
+                  location.pathname === "/science"
+                    ? "text-blue-700 dark:text-blue-500"
+                    : "text-gray-900 dark:text-white"
+                } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
               >
                 Science
               </Link>
@@ -84,7 +109,11 @@ export default function Navbar(props) {
             <li>
               <Link
                 to="/sports"
-                className={`block py-2 px-3 rounded ${location.pathname === '/sports' ? 'text-blue-700 dark:text-blue-500' : 'text-gray-900 dark:text-white'} hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
+                className={`block py-2 px-3 rounded ${
+                  location.pathname === "/sports"
+                    ? "text-blue-700 dark:text-blue-500"
+                    : "text-gray-900 dark:text-white"
+                } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
               >
                 Sports
               </Link>
@@ -92,7 +121,11 @@ export default function Navbar(props) {
             <li>
               <Link
                 to="/technology"
-                className={`block py-2 px-3 rounded ${location.pathname === '/technology' ? 'text-blue-700 dark:text-blue-500' : 'text-gray-900 dark:text-white'} hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
+                className={`block py-2 px-3 rounded ${
+                  location.pathname === "/technology"
+                    ? "text-blue-700 dark:text-blue-500"
+                    : "text-gray-900 dark:text-white"
+                } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}
               >
                 Technology
               </Link>
@@ -103,7 +136,7 @@ export default function Navbar(props) {
                 onClick={toggleDropdown}
                 className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
               >
-                Dropdown
+                Countries
                 <svg
                   className="w-2.5 h-2.5 ml-2.5"
                   aria-hidden="true"
@@ -121,16 +154,46 @@ export default function Navbar(props) {
                 </svg>
               </button>
               {dropdownOpen && (
-                <div id="dropdownNavbar" className="absolute z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownNavbarLink">
+                <div
+                  id="dropdownNavbar"
+                  className="absolute z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+                >
+                  <ul
+                    className="py-2 text-sm text-gray-700 dark:text-gray-400"
+                    aria-labelledby="dropdownNavbarLink"
+                  >
                     <li>
-                      <button className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>{props.setCountry('in')}}>India</button>
+                      <button
+                        className="container px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={() => {
+                          props.setCountry("in");
+                          setDropdownOpen(false);
+                        }}
+                      >
+                        India
+                      </button>
                     </li>
                     <li>
-                    <button className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>{props.setCountry('us')}}>US</button>
+                      <button
+                        className="container px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={() => {
+                          props.setCountry("us");
+                          setDropdownOpen(false);
+                        }}
+                      >
+                        US
+                      </button>
                     </li>
                     <li>
-                      <button className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>{props.setCountry('ua')}}>UA</button>
+                      <button
+                        className="container px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={() => {
+                          props.setCountry("ua");
+                          setDropdownOpen(false);
+                        }}
+                      >
+                        UA
+                      </button>
                     </li>
                   </ul>
                 </div>
@@ -140,5 +203,5 @@ export default function Navbar(props) {
         </div>
       </div>
     </nav>
-  )
+  );
 }
